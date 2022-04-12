61107742c1547a20bc11007ea448677bbaa23602
Glitch (hello-webpage) <none>
2022-04-12 22:57:15 +0000

    👓🌥 Checkpoint ./server.js:987258/2096

--- server.js
+++ server.js
@@ -35,32 +35,42 @@ app.get("/output.asp", function (request, response) {
   var content = fs.readFileSync('./views/output.html', 'utf8');
 
   // read variables
-  let texta = request.query.texta;
-  let textb = request.query.textb;
-  let textc = request.query.textc;
-  let textd = request.query.textd;
-  let texte = request.query.texte;
-  let textf = request.query.textf;
-  let textg = request.query.textg;
+  let texta = request.query.texta ? request.query.texta : '';
+  let textb = request.query.textb ? request.query.textb : '';
+  let textc = request.query.textc ? request.query.textc : '';
+  let textd = request.query.textd ? request.query.textd : '';
+  let texte = request.query.texte ? request.query.texte : '';
+  let textf = request.query.textf ? request.query.textf : '';
+  let textg = request.query.textg ? request.query.textg : '';
+  let texth = request.query.texth ? request.query.texth : '';
   
   let boxa = request.query.boxa ? `<img src="/assets/${request.query.boxa}" />` : '';
-  let boxb = request.query.boxb;
-  let boxc = request.query.boxc;
-  let boxd = request.query.boxd;
-  let boxe = request.query.boxe;
-  let boxf = request.query.boxf;
-  let boxg = request.query.boxg;
+  let boxb = request.query.boxb ? `<img src="/assets/${request.query.boxb}" />` : '';;
+  let boxc = request.query.boxc ? `<img src="/assets/${request.query.boxc}" />` : '';;
+  let boxd = request.query.boxd ? `<img src="/assets/${request.query.boxd}" />` : '';;
+  let boxe = request.query.boxe ? `<img src="/assets/${request.query.boxe}" />` : '';;
+  let boxf = request.query.boxf ? `<img src="/assets/${request.query.boxf}" />` : '';;
+  let boxg = request.query.boxg ? `<img src="/assets/${request.query.boxg}" />` : '';;
+  let boxh = request.query.boxh ? `<img src="/assets/${request.query.boxh}" />` : '';;
 
   // replace string
-  content = texta ? content.replace("${texta}", texta) : content;
-  content = textb ? content.replace("${textb}", textb);
+  content = content.replace("${texta}", texta);
+  content = content.replace("${textb}", textb);
   content = content.replace("${textc}", textc);
   content = content.replace("${textd}", textd);
   content = content.replace("${texte}", texte);
   content = content.replace("${textf}", textf);
   content = content.replace("${textg}", textg);
+  content = content.replace("${texth}", texth);
   
   content = content.replace("${boxa}", boxa);
+  content = content.replace("${boxb}", boxb);
+  content = content.replace("${boxc}", boxc);
+  content = content.replace("${boxd}", boxd);
+  content = content.replace("${boxe}", boxe);
+  content = content.replace("${boxf}", boxf);
+  content = content.replace("${boxg}", boxg);
+  content = content.replace("${boxh}", boxh);
 
   // return raw html
   response.set('Content-Type', 'text/html');
